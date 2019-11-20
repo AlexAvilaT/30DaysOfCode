6b60235a2559cf841f896956b1d40727ad4344fa
AlexAvilaT <alex.avilat6@gmail.com>
2019-11-19 18:56:38 -0600

    day 2 and day 5

---
+++ src/com/Day30s/app/Day2.java
@@ -0,0 +1,33 @@
+package com.Day30s.app;
+import java.io.*;
+import java.util.*;
+/*Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!*/
+public class Day2 
+{
+
+	public static void main(String[] args) 
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+		Scanner scan = new Scanner(System.in);
+        String n = scan.nextLine();
+
+
+        System.out.println(scan.hasNext());
+
+        System.out.println(scan.nextline());
+
+        System.out.println(scan.hasNext());
+
+
+        scan.close;
+
+
+    }
+	
+	
+	
+	
+}
